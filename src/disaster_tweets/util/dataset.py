c3f48b3ea9d7c38d0635a511f4fe1afba592e47a
Derek Haynes <derek.haynes@gmail.com>
2020-05-27 07:59:46 -0600

    Updating data dir

--- src/disaster_tweets/util/dataset.py
+++ src/disaster_tweets/util/dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import whisk
+import disaster_tweets
 
 def dataset_to_df():
-    tweet= pd.read_csv(whisk.data_dir / 'raw/train.csv')
-    test=pd.read_csv(whisk.data_dir / 'raw/test.csv')
+    tweet= pd.read_csv(disaster_tweets.data_dir / 'raw/train.csv')
+    test=pd.read_csv(disaster_tweets.data_dir / 'raw/test.csv')
     return [tweet,test]
